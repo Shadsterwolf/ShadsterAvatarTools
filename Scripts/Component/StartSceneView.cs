73f3db14553232e51dc6ff1ac6d20d437cda8b1f
Shadsterwolf <shadsterwolf1@gmail.com>
2022-07-06 14:46:29 -0400

    0.4.1

--- Scripts/Component/StartSceneView.cs
+++ Scripts/Component/StartSceneView.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class StartSceneView : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+#if UNITY_EDITOR
         SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+#endif
     }
 }
